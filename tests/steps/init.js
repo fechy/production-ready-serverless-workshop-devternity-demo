fc7a177e34fb8995639da07761c573f4b308eec8
Fernando Giovanini <fernando.giovanini@coliquio.de>
2018-12-01 13:19:35 +0100

    Forced id

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -5,9 +5,8 @@ const init = async () => {
     return
   }
 
-  process.env.BUILD_ID = "6u5zv4brql";
-  process.env.TEST_ROOT = `https://${process.env.BUILD_ID}.execute-api.eu-west-1.amazonaws.com/dev`
-  process.env.restaurants_api   = `https://${process.env.BUILD_ID}.execute-api.eu-west-1.amazonaws.com/dev/restaurants`
+  process.env.TEST_ROOT = `https://6u5zv4brql.execute-api.eu-west-1.amazonaws.com/dev`
+  process.env.restaurants_api   = `https://6u5zv4brql.execute-api.eu-west-1.amazonaws.com/dev/restaurants`
   process.env.restaurants_table = "restaurants-dev-fgiovanini"
   process.env.AWS_REGION        = "eu-west-1"
   process.env.restaurant_notification_topic = 'restaurants-dev-fgiovanini'
