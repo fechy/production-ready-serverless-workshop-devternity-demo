f56cbb8cea594dffd32fded1a1fb70a77c434915
Fernando Giovanini <fernando.giovanini@coliquio.de>
2018-12-01 13:14:06 +0100

    Fixed?

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -5,8 +5,9 @@ const init = async () => {
     return
   }
 
-  process.env.TEST_ROOT = "https://q67v7nz0al.execute-api.eu-west-1.amazonaws.com/dev"
-  process.env.restaurants_api   = "https://q67v7nz0al.execute-api.eu-west-1.amazonaws.com/dev/restaurants"
+  process.env.BUILD_ID = "6u5zv4brql";
+  process.env.TEST_ROOT = `https://${process.env.BUILD_ID}.execute-api.eu-west-1.amazonaws.com/dev`
+  process.env.restaurants_api   = `https://${process.env.BUILD_ID}.execute-api.eu-west-1.amazonaws.com/dev/restaurants`
   process.env.restaurants_table = "restaurants-dev-fgiovanini"
   process.env.AWS_REGION        = "eu-west-1"
   process.env.restaurant_notification_topic = 'restaurants-dev-fgiovanini'
